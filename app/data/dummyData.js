316f287a2b427734377e76cdca552d77cfaf17ec
DParra657 <Diegoparra657@gmail.com>
2025-04-22 22:02:09 -0400

    Your commit message

--- app/data/dummyData.js
+++
@@ -1,5 +0,0 @@
-export const items = [
-    { id: 1, title: "Mini Fridge", image: "/images/fridge.jpg" },
-    { id: 2, title: "Desk Lamp", image: "/images/lamp.jpg" },
-    { id: 3, title: "Textbooks", image: "/images/books.jpg" },
-];
